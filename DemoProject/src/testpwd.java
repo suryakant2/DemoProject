e070f8f0fb8e8a907e9eed48d2075bb6c8bd5f21
shashikant <shashikant@LAPTOP-P67BNFM1>
2019-02-05 14:20:14 +0530

    add test file

---
+++ DemoProject/src/testpwd.java
@@ -0,0 +1,49 @@
+
+
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+
+
+
+public class testpwd {
+
+	public static void main(String[] args) {
+		
+		//test comment for git test
+		// TODO Auto-generated method stub
+		//CipherUtils ax = new CipherUtils
+		System.out.println("hello moto");
+		System.out.println("test4");
+		System.out.println("Git test");
+		
+		// 3 years, 2 months, 1 day
+	//	Period period = Period.of(3, 2, 1);
+	//	LocalDate oldDate= new LocalDate();
+		// Calendar object
+		Calendar cal = Calendar.getInstance();
+      
+       
+		// "move" cal to monday this week (i understand it this way)
+       cal.set(Calendar.DAY_OF_WEEK, new Date().getDay());
+
+		// calculate monday week ago (moves cal 7 days back)
+        cal.add(Calendar.DATE, -6);
+       
+		Date firstDateOfPreviousWeek = cal.getTime();
+
+		// calculate sunday last week (moves cal 6 days fwd)
+		cal.add(Calendar.DATE, 6);
+		Date lastDateOfPreviousWeek = cal.getTime();
+		
+		System.out.println(firstDateOfPreviousWeek);
+		System.out.println(lastDateOfPreviousWeek);
+		
+			}
+
+}
